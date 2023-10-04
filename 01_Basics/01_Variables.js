7c39361bb538e3ede191f83a6de2b11eb7b5a818
gaurav2dev <131958404+gaurav2dev@users.noreply.github.com>
2023-10-04 09:31:35 +0000

    new variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,6 @@
+const account_Id = "452AXY"
+const account_Email = "xyz_abc@google.com"
+const account_Password = "1029384756"
+const account_City = "Mumbai"
+console.table([`ID:- ${account_Id}`,`Email:- ${account_Email}`,`Password:- ${account_Password}`,`City:- ${account_City}`])
+
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,2 @@
 console.log('Hello Buddy Welcome Back.....')
+console.log("heyy");
