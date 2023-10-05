0845f5a79d0b00837dfea025e9551fab928ee779
gaurav2dev <131958404+gaurav2dev@users.noreply.github.com>
2023-10-05 10:34:21 +0000

    json and new file changes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = function age(){
+    return 0
+}
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = "Hello"
+let typeofsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof(typeofsLoggedIn))
+console.log(typeofsLoggedIn)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+// undefined => NaN
+// null => 0
+// function => NaN
+
