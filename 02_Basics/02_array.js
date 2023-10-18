e51b4541b2f1275231e48e7fcd3892bec42189e4
gaurav2dev <131958404+gaurav2dev@users.noreply.github.com>
2023-10-18 16:36:05 +0530

    Array Part - 2

---
+++ 02_Basics/02_array.js
@@ -0,0 +1,35 @@
+// const heros = ["ironman", "superman", "batman"];
+// const players = ["Dhoni", "Rohit", "Virat"];
+// const ranger = ["red", "blue", "green"];
+// const newArr = [...players, ...heros, ...ranger];
+// console.log(newArr);
+
+// const newArray = heros.concat(players);
+// console.log(newArray);
+
+// heros.push(players);
+// console.log(heros);
+// const d = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6],
+//   [[7, 8], [[[[4, 5, 6, [[[89, 80]]]]]]]],
+// ];
+// console.log(d.flat(Infinity));
+// console.log(
+//   Array.isArray(
+//     "2D arrays are useful for representing grids, tables, matrices, and more. They are commonly used in applications involving games, data visualization, and data manipulation."
+//   )
+// );
+// console.log(Array.from("2D arrays are useful for"));
+// for (let index = 0; index < d.length; index++) {
+//   console.log(index);
+// }
+// console.log(d.length);
+
+console.log(Array.from({ name: "hitesh" }));
+
+const a = 3444;
+const b = 5666;
+const c = 8900;
+console.log(Array.of(a, b, c));
