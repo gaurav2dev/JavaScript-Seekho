eebb55883a5bd86fdc3cd2893871423ce886f249
gaurav2dev <131958404+gaurav2dev@users.noreply.github.com>
2023-12-20 21:28:13 +0530

    Project to VS Code

---
+++ 07_Projects/1-colorChanger/chaiaurcode.js
@@ -0,0 +1,43 @@
+const buttons = document.querySelectorAll(".button");
+console.log(buttons);
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellowgreen") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
+
+// // Second Solution(Using Switch Case)
+// const cool = e.target.id;
+// switch (cool) {
+//   case "grey":
+//     body.style.backgroundColor = "grey";
+//     break;
+//   case "white":
+//     body.style.backgroundColor = "white";
+//     break;
+//   case "blue":
+//     body.style.backgroundColor = "blue";
+//     break;
+//   case "yellow":
+//     body.style.backgroundColor = "yellow";
+//     break;
+//   default:
+//     body.style.backgroundColor = "yellowgreen";
+//     break;
+// }
